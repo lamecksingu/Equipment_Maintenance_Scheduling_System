3e8222a7be46ae0714b98f69bf18cafb3c994ada
lamecksingu <singulameck9619@gmail.com>
2024-09-20 20:32:17 +0300

    Update to the user handling logic on the backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,6 +53,9 @@ exports.loginUser = (req, res) => {
 			const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
 				expiresIn: '1h'
 			});
+
+			// Log the generated token for debbuging
+			console.log("Generated Token:", token);
 			res.status(200).json({ token });
 		}
 	});
